194810e6e6a585bfc30417682a625dc48c755f57
Noirox1 <lmb138@gmx.de>
2023-09-30 18:41:58 +0200

    Death Flash für Julian

--- LudumDare54/Assets/PlayerMovement.cs
+++ LudumDare54/Assets/PlayerMovement.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float acc;
     [SerializeField] private float dec;
+    [SerializeField] Image redFlashImage;
     private GameObject player;
     private Rigidbody2D rb;
     private Vector2 move;
@@ -22,6 +25,9 @@ public class PlayerMovement : MonoBehaviour
         sneak = Input.GetKey(KeyCode.LeftShift);
 
         print(rb.velocity.magnitude);
+        if (Input.GetKeyDown(KeyCode.G)) {
+            Die();
+        }
     }
     void FixedUpdate() {
         if(move.magnitude > 0) {
@@ -40,7 +46,27 @@ public class PlayerMovement : MonoBehaviour
             rb.drag = dec;
         }
     }
-    void Die() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    public void Die() {
+	    StartCoroutine(redFlash());
+	    // Do more dying...
+    }
+
+    IEnumerator redFlash() {
+        redFlashImage.gameObject.SetActive(true);
+        int stepCount = 20;
+	    for (int i = 0; i < stepCount; i++) {
+            Color c = redFlashImage.color;
+            Debug.Log("Adding "+(1f/stepCount));
+	        c.a += (float)(1f/stepCount);
+            redFlashImage.color = c;
+            yield return new WaitForSeconds(0.01f);
+        }
+        for (int i = 0; i < stepCount; i++) {
+            Color c = redFlashImage.color;
+	        c.a -= (float)(1f/stepCount);
+            redFlashImage.color = c;
+            yield return new WaitForSeconds(0.01f);
+        }
+        redFlashImage.gameObject.SetActive(false);
     }
 }
