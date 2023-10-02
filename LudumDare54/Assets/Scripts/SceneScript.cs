3aad318f9d94a64e9ac8040a5ef9bac5dce17104
Noirox1 <lmb138@gmx.de>
2023-10-02 21:59:19 +0200

    Ende Scene, Death Button, Lastlevel tracking,

--- LudumDare54/Assets/Scripts/SceneScript.cs
+++ LudumDare54/Assets/Scripts/SceneScript.cs
@@ -1,20 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
 public class SceneScript : MonoBehaviour
 {
     [SerializeField] string sceneName = "";
+    string[] levelNames = {"StartFR", "Area2"};
 
     public void loadSceneDirect(String sceneNameDirect) {
         Debug.Log("Attempting to load Scene " + '\u0022'+sceneNameDirect+'\u0022' + "..."); 
+        trackScene(sceneNameDirect);
         SceneManager.LoadScene(sceneNameDirect);
     } 
     public void loadScene() {
         Debug.Log("Attempting to load Scene " + '\u0022'+sceneName+'\u0022' + "..."); 
-        SceneManager.LoadScene(sceneName);
+        loadSceneDirect(sceneName);
     }
     public void Adios() {
         Application.Quit();
@@ -22,6 +25,18 @@ public class SceneScript : MonoBehaviour
     void OnTriggerEnter2D(Collider2D c) {
         if(c.CompareTag("Player")) {
             loadScene();
+        }
+    }
+    void trackScene(string newScene) {
+        if(levelNames.Contains(newScene)) {
+            STATICStatTracker.lastLevel = newScene;
+        }
+    }
+    public void ReloadLastVisitedLevel() {
+        if(STATICStatTracker.lastLevel != "") {
+            SceneManager.LoadScene(STATICStatTracker.lastLevel);
+        } else {
+            SceneManager.LoadScene("MainMenu");
         }
     }
 }
